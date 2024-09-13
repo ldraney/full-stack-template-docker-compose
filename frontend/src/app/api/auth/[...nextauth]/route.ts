7a1f1b1fc44fcb60389a016452857e13cd5f7789
ldraney <draneylucas@gmail.com>
2024-09-13 13:57:55 -0600

    dashboard working!

--- frontend/src/app/api/auth/[...nextauth]/route.ts
+++ frontend/src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,9 @@
+// src/app/api/auth/[...nextauth]/route.ts
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
+import type { NextAuthOptions } from 'next-auth'
 
-const handler = NextAuth({
+export const authOptions: NextAuthOptions = {
 	providers: [
 		GoogleProvider({
 			clientId: process.env.GOOGLE_CLIENT_ID!,
@@ -9,6 +11,8 @@ const handler = NextAuth({
 		}),
 	],
 	// Add any additional NextAuth configuration here
-})
+}
+
+const handler = NextAuth(authOptions)
 
 export { handler as GET, handler as POST }
